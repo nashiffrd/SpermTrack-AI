3141a0889f898be39f4611944923e5a8c7064e7b
Muhammad Nashif Farid <134785469+nashiffrd@users.noreply.github.com>
2026-02-11 11:55:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -128,14 +128,14 @@ with tab2:
         if st.session_state.prepared_video is not None:
             st.write("### Visualisasi Tahap A (Preprocessing)")
             # Visualisasi (Asli > Gray > Contrast)
-                cap = cv2.VideoCapture(tfile.name)
-                ret, frame = cap.read()
-                if ret:
-                    c1, c2, c3 = st.columns(3)
-                    c1.image(frame, caption="Frame Asli", use_container_width=True)
-                    c2.image(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), caption="Grayscale", use_container_width=True)
-                    c3.image(cv2.convertScaleAbs(frame, alpha=1.5, beta=10), caption="Contrast", use_container_width=True)
-                cap.release()
+            cap = cv2.VideoCapture(tfile.name)
+            ret, frame = cap.read()
+            if ret:
+                c1, c2, c3 = st.columns(3)
+                c1.image(frame, caption="Frame Asli", use_container_width=True)
+                c2.image(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), caption="Grayscale", use_container_width=True)
+                c3.image(cv2.convertScaleAbs(frame, alpha=1.5, beta=10), caption="Contrast", use_container_width=True)
+            cap.release()
 
         # --- TAMPILAN TAHAP B (Data Tracking) ---
         if st.session_state.tracks_df is not None:
